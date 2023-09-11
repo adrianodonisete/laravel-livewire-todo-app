ca8aca56a46a6151a55b905162656c412cf561af
Adriano Machado <adrianodonisete@gmail.com>
2023-09-11 18:05:03 -0300

    add features read and create

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -2,12 +2,43 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Rule;
 use Livewire\Component;
+use Livewire\WithPagination;
+
+use App\Models\Todo;
 
 class TodoList extends Component
 {
+    use WithPagination;
+
+    #[Rule('required|min:3|max:50')]
+    public string $name;
+
+    public string $search = '';
+
+    public function create(array $args = []): void
+    {
+        $validated =  $this->validateOnly('name');
+
+        Todo::create($validated);
+
+        $this->reset('name');
+
+        session()->flash('success', 'Created.');
+    }
+
     public function render()
     {
-        return view('livewire.todo-list');
+        $all = Todo::latest()
+            ->where('name', 'like', "%{$this->search}%")
+            ->paginate(4);
+
+        return view(
+            'livewire.todo-list',
+            [
+                'todos' => $all
+            ]
+        );
     }
 }
